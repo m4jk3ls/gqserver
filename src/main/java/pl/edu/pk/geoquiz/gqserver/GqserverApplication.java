30b6ca78c75c832d4d0db9f568dc8b665fefc1c9
Michal Sroka <m4jk3ls@gmail.com>
2018-01-31 17:51:39 +0100

    Initial commit

---
+++ src/main/java/pl/edu/pk/geoquiz/gqserver/GqserverApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.pk.geoquiz.gqserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GqserverApplication.class, args);
+	}
+}
