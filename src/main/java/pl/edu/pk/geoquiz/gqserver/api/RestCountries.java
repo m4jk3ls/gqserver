ff3c24668d65194e3957147c8201cdb4a265ac56
Michal Sroka <m4jk3ls@gmail.com>
2018-02-20 20:05:53 +0100

    Getting data from API for specific question

--- src/main/java/pl/edu/pk/geoquiz/gqserver/api/RestCountries.java
+++ src/main/java/pl/edu/pk/geoquiz/gqserver/api/RestCountries.java
@@ -34,28 +34,119 @@ public class RestCountries {
 		}).getBody();
 
 		List<Map<String, String>> randomCountries = new ArrayList<>();
+		List<String> answersForCheck = new ArrayList<>();
 		do {
 			Map<String, Object> firstCountry = allCountries.get(new Random().nextInt(allCountries.size()));
 			Map<String, String> mapToInsert = new HashMap<>();
-			mapToInsert.put("name", (String) firstCountry.get("name"));
+			mapToInsert.put("name", firstCountry.get("name").toString());
 
-			if (firstCountry.get(field) instanceof List && !((List) firstCountry.get(field)).isEmpty() && firstCountry.get(field) != null && secondField != null) {
-				List<Map<String, String>> firstCountryFieldHowList = (List<Map<String, String>>) firstCountry.get(field);
-				Map<String, String> randomMap = firstCountryFieldHowList.get(new Random().nextInt(firstCountryFieldHowList.size()));
+			if (isFieldListOfMapsWithContent(firstCountry, field) && secondField != null) {
+				List<Map<String, String>> firstCountryFieldHowMapList = (List<Map<String, String>>) firstCountry.get(field);
+				for (Map<String, String> curr : firstCountryFieldHowMapList) {
+					answersForCheck.add(curr.get(secondField));
+				}
+				Map<String, String> randomMap = firstCountryFieldHowMapList.get(new Random().nextInt(firstCountryFieldHowMapList.size()));
 				mapToInsert.put(field, randomMap.get(secondField));
 				randomCountries.add(mapToInsert);
-			} else if (!(firstCountry.get(field) instanceof List) && firstCountry.get(field) != null && secondField == null) {
-				mapToInsert.put(field, (String) firstCountry.get(field));
+			} else if (isFieldListOfStringsWithContent(firstCountry, field) && secondField == null) {
+				List<String> firstCountryFieldHowStringList = (List<String>) firstCountry.get(field);
+				answersForCheck.addAll(firstCountryFieldHowStringList);
+				mapToInsert.put(field, firstCountryFieldHowStringList.get(new Random().nextInt(firstCountryFieldHowStringList.size())));
+				randomCountries.add(mapToInsert);
+			} else if (isntFieldList(firstCountry, field) && secondField == null) {
+				mapToInsert.put(field, firstCountry.get(field).toString());
 				randomCountries.add(mapToInsert);
 			}
 		} while (randomCountries.isEmpty());
 
-		/*Teraz trzeba bedzie losowac randomowy kraj, nastepnie sprawdzac, czy jego nazwa nie duplikuje sie z nazwa juz istniejaca w liscie randomCountries.
-		 * Jesli warunek zostanie spelniony, to kolejnym krokiem bedzie sprawdzenie kazdego np. jezyka nowo wylosowanego panstwa, czy aby nie duplikuje sie
-		 * z juz isniejacym w randomCountries. Jesli drugi z wymienionych warunkow jest spelniony, to wylosowane panstwo wraz np. ze swoim jezykem nadaje
-		 * sie do wstawienia do listy randomCountries. Jesli proba znalezienia takiego panstwa zakonczy sie fiaskiem, to nie robic nic i wylosowac nowe
-		 * panstwo (analogicznie do powyzszej konstrukcji, ktora szuka pierwszego panstwa do listy randomCountries).*/
-
+		while (randomCountries.size() < numberOfCountries) {
+			Map<String, Object> anotherCountry = allCountries.get(new Random().nextInt(allCountries.size()));
+			if (isFieldListOfMapsWithContent(anotherCountry, field) && secondField != null) {
+				String potentialCandidate = null;
+				List<Map<String, String>> anotherCountryFieldHowList = (List<Map<String, String>>) anotherCountry.get(field);
+				for (Map<String, String> newField : anotherCountryFieldHowList) {
+					boolean isValid = true;
+					for (String curr : answersForCheck) {
+						if (newField.get(secondField).equals(curr)) {
+							isValid = false;
+						}
+					}
+					if (isValid) {
+						potentialCandidate = newField.get(secondField);
+						break;
+					}
+				}
+				if (potentialCandidate != null) {
+					boolean uniqueCountryName = true;
+					for (Map<String, String> currCountry : randomCountries) {
+						if (anotherCountry.get("name").equals(currCountry.get("name"))) {
+							uniqueCountryName = false;
+						}
+					}
+					if (uniqueCountryName) {
+						save(answersForCheck, potentialCandidate, anotherCountry.get("name").toString(), field, randomCountries);
+					}
+				}
+			} else if (isFieldListOfStringsWithContent(anotherCountry, field) && secondField == null) {
+				boolean uniqueCountryName = true;
+				for (Map<String, String> currCountry : randomCountries) {
+					if (anotherCountry.get("name").equals(currCountry.get("name"))) {
+						uniqueCountryName = false;
+					}
+				}
+				if (uniqueCountryName) {
+					List<String> anotherCountryFieldHowStringList = (List<String>) anotherCountry.get(field);
+					for (String curr1 : anotherCountryFieldHowStringList) {
+						boolean isValid = true;
+						for (String curr2 : answersForCheck) {
+							if (curr1.equals(curr2)) {
+								isValid = false;
+							}
+						}
+						if (isValid) {
+							save(answersForCheck, curr1, anotherCountry.get("name").toString(), field, randomCountries);
+						}
+					}
+				}
+			} else if (isntFieldList(anotherCountry, field) && secondField == null) {
+				Map<String, String> potentialMapToInsert = new HashMap<>();
+				potentialMapToInsert.put("name", anotherCountry.get("name").toString());
+				potentialMapToInsert.put(field, anotherCountry.get(field).toString());
+				boolean countryIsValid = true;
+				for (Map<String, String> currCountry : randomCountries) {
+					if (currCountry.get("name").equals(potentialMapToInsert.get("name")) ||
+							currCountry.get(field).equals(potentialMapToInsert.get(field))) {
+						countryIsValid = false;
+					}
+				}
+				if (countryIsValid) {
+					randomCountries.add(potentialMapToInsert);
+				}
+			}
+		}
 		return randomCountries;
+	}
+
+	private void save(List<String> answersForCheck, String answerToSave, String countryName,
+					  String field, List<Map<String, String>> countryListForClient) {
+		answersForCheck.add(answerToSave);
+		Map<String, String> mapToInsert = new HashMap<>();
+		mapToInsert.put("name", countryName);
+		mapToInsert.put(field, answerToSave);
+		countryListForClient.add(mapToInsert);
+	}
+
+	private boolean isFieldListOfMapsWithContent(Map<String, Object> map, String key) {
+		return (map.get(key) instanceof List && !((List) map.get(key)).isEmpty() &&
+				map.get(key) != null && ((List) map.get(key)).get(0) instanceof Map);
+	}
+
+	private boolean isFieldListOfStringsWithContent(Map<String, Object> map, String key) {
+		return (map.get(key) instanceof List && !((List) map.get(key)).isEmpty() &&
+				map.get(key) != null && ((List) map.get(key)).get(0) instanceof String);
+	}
+
+	private boolean isntFieldList(Map<String, Object> map, String key) {
+		return (!(map.get(key) instanceof List) && map.get(key) != null);
 	}
 }
